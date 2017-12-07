8feb61a221c2eae0fdbe7217fb365fe2fe614fac
Tony Xing <tonyxwz@yahoo.com>
2017-12-07 01:10:59 +0100

    New mip function using gap sampling
    `RaycastRenderer.getRayDepth` can be reused in compositing and 2-D

--- src/volvis/RaycastRenderer.java
+++ src/volvis/RaycastRenderer.java
@@ -56,7 +56,7 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
         image = new BufferedImage(imageSize, imageSize, BufferedImage.TYPE_INT_ARGB);
         
         // An image on which the volume may be rendered. It is scaled to the image field
-        createNativeImage();
+        // createNativeImage();
         
         // create a standard TF where lowest intensity maps to black, the highest to white, and opacity increases
         // linearly from 0.0 to 1.0 over the intensity range
@@ -159,12 +159,50 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
 
     }
 
+    // viewVec, coordinate on view plane
+    double[] getRayDepth(double[] viewVec, double[] coordOnPlane){
+        int[][] border = new int[3][2];
+        border[0][0] = 0; // x
+        border[0][1] = volume.getDimX();
+        border[1][0] = 0; // y
+        border[1][1] = volume.getDimY();
+        border[2][0] = 0; // z
+        border[2][1] = volume.getDimZ();
+        
+        double fDepth = Double.POSITIVE_INFINITY;
+        double bDepth = Double.NEGATIVE_INFINITY;
+        
+        // traverse over the 3 directions
+        for (int d = 0; d < 3; d++){
+            if (viewVec[d] != 0){
+                // one of dp and dn is negative
+                double dp = (border[d][0] - coordOnPlane[d]) / viewVec[d];
+                double dn = (border[d][1] - coordOnPlane[d]) / viewVec[d];
+                
+                if (dp < 0 || dn > 0){
+                    double tmp = dp;
+                    dp = dn;
+                    dn = tmp;
+                }
+                
+                if (dp < fDepth)
+                    fDepth = dp;
+                
+                if (dn > bDepth)
+                    bDepth = dn;
+            }
+            
+        }
+        double[] fbDepth = new double[] {fDepth, bDepth};
+        // System.out.println("getRayDepth::fbDepth " + fbDepth);
+        return fbDepth;
+    }
     
     void mip(double[] viewMatrix){
         // clear image
-        for (int j = 0; j < nativeImage.getHeight(); j++) {
-            for (int i = 0; i < nativeImage.getWidth(); i++) {
-                nativeImage.setRGB(i, j, 0);
+        for (int j = 0; j < image.getHeight(); j++) {
+            for (int i = 0; i < image.getWidth(); i++) {
+                image.setRGB(i, j, 0);
             }
         }
 
@@ -178,10 +216,12 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
         VectorMath.setVector(vVec, viewMatrix[1], viewMatrix[5], viewMatrix[9]);
         
         // image is square
-        int imageCenter = nativeImage.getWidth() / 2;
-
-        double[] originPlaneCoord = new double[3];
-        double[] pixelCoord = new double[3];
+        int imageCenter = image.getWidth() / 2;
+        
+        // coordinate of point (i,j) in the coordinate system of the volume
+        double[] coordOnPlane = new double[3];
+        // coordinate of point on the ray casting line
+        double[] voxelCoord = new double[3];
         double[] volumeCenter = new double[3];
         VectorMath.setVector(volumeCenter, volume.getDimX() / 2, volume.getDimY() / 2, volume.getDimZ() / 2);
         
@@ -190,24 +230,24 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
         TFColor voxelColor = new TFColor();
 
         
-        for (int j = 0; j < nativeImage.getHeight(); j++) {
-            for (int i = 0; i < nativeImage.getWidth(); i++) {
+        for (int j = 0; j < image.getHeight(); j++) {
+            for (int i = 0; i < image.getWidth(); i++) {
                 int val = 0;
                 
-                int depth = Math.max(volume.getDimX(), Math.max(volume.getDimY(), volume.getDimZ()));
-                originPlaneCoord[0] = uVec[0] * (i - imageCenter) * (1/renderScale) + vVec[0] * (j - imageCenter) * (1/renderScale)
-                            + volumeCenter[0];
-                originPlaneCoord[1] = uVec[1] * (i - imageCenter) * (1/renderScale) + vVec[1] * (j - imageCenter) * (1/renderScale)
-                            + volumeCenter[1];
-                originPlaneCoord[2] = uVec[2] * (i - imageCenter) * (1/renderScale) + vVec[2] * (j - imageCenter) * (1/renderScale)
-                            + volumeCenter[2];
+                coordOnPlane[0] = uVec[0] * (i - imageCenter) + vVec[0] * (j - imageCenter) + volumeCenter[0];
+                coordOnPlane[1] = uVec[1] * (i - imageCenter) + vVec[1] * (j - imageCenter) + volumeCenter[1];
+                coordOnPlane[2] = uVec[2] * (i - imageCenter) + vVec[2] * (j - imageCenter) + volumeCenter[2];
+                
+                double[] fbDepth = getRayDepth(viewVec, coordOnPlane);
+                double gap = (fbDepth[0] - fbDepth[1]) / sampleNum;
+                // System.out.println("mip::gap: " + gap);
+                // ray cast through (i,j)     
+                for (double k = fbDepth[1]; k <= fbDepth[0]; k += gap) {
+                    voxelCoord[0] = coordOnPlane[0] + viewVec[0] * k;
+                    voxelCoord[1] = coordOnPlane[1] + viewVec[1] * k;
+                    voxelCoord[2] = coordOnPlane[2] + viewVec[2] * k;
                     
-                for (double k = -depth; k <= depth; k += 5/renderScale) {
-                    pixelCoord[0] = originPlaneCoord[0] + viewVec[0] * k;
-                    pixelCoord[1] = originPlaneCoord[1] + viewVec[1] * k;
-                    pixelCoord[2] = originPlaneCoord[2] + viewVec[2] * k;
-                    
-                    val = Math.max(val, getVoxel(pixelCoord));
+                    val = Math.max(val, getVoxel(voxelCoord));
                 }
                 
                 // Map the intensity to a grey value by linear scaling
@@ -225,106 +265,100 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
                 int c_green = voxelColor.g <= 1.0 ? (int) Math.floor(voxelColor.g * 255) : 255;
                 int c_blue = voxelColor.b <= 1.0 ? (int) Math.floor(voxelColor.b * 255) : 255;
                 int pixelColor = (c_alpha << 24) | (c_red << 16) | (c_green << 8) | c_blue;
-                nativeImage.setRGB(i, j, pixelColor);
+                image.setRGB(i, j, pixelColor);
             }
         }
-        
-        AffineTransform at = new AffineTransform();
-        at.scale(1/renderScale, 1/renderScale);
-        AffineTransformOp scaleOp = new AffineTransformOp(at, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-        
-        scaleOp.filter(nativeImage, image);
     }
     
     
-    void compositing(double[] viewMatrix){
-        // clear image
-        for (int j = 0; j < nativeImage.getHeight(); j++) {
-            for (int i = 0; i < nativeImage.getWidth(); i++) {
-                nativeImage.setRGB(i, j, 0);
-            }
-        }
-
-        // vector uVec and vVec define a plane through the origin, 
-        // perpendicular to the view vector viewVec
-        double[] viewVec = new double[3];
-        double[] uVec = new double[3];
-        double[] vVec = new double[3];
-        VectorMath.setVector(viewVec, viewMatrix[2], viewMatrix[6], viewMatrix[10]);
-        VectorMath.setVector(uVec, viewMatrix[0], viewMatrix[4], viewMatrix[8]);
-        VectorMath.setVector(vVec, viewMatrix[1], viewMatrix[5], viewMatrix[9]);
-        
-        // image is square
-        int imageCenter = nativeImage.getWidth() / 2;
-
-        double[] originPlaneCoord = new double[3];
-        double[] pixelCoord = new double[3];
-        double[] volumeCenter = new double[3];
-        double[] pixelValues = new double[nativeImage.getHeight() * nativeImage.getWidth()];
-        VectorMath.setVector(volumeCenter, volume.getDimX() / 2, volume.getDimY() / 2, volume.getDimZ() / 2);
-        
-        // sample on a plane through the origin of the volume data
-        TFColor voxelColor = new TFColor();
-
-        
-        for (int j = 0; j < nativeImage.getHeight(); j++) {
-            for (int i = 0; i < nativeImage.getWidth(); i++) {
-                double val = 0;
-                
-                int depth = Math.max(volume.getDimX(), Math.max(volume.getDimY(), volume.getDimZ()));
-                originPlaneCoord[0] = uVec[0] * (i - imageCenter) * (1/renderScale) + vVec[0] * (j - imageCenter) * (1/renderScale)
-                            + volumeCenter[0];
-                originPlaneCoord[1] = uVec[1] * (i - imageCenter) * (1/renderScale) + vVec[1] * (j - imageCenter) * (1/renderScale)
-                            + volumeCenter[1];
-                originPlaneCoord[2] = uVec[2] * (i - imageCenter) * (1/renderScale) + vVec[2] * (j - imageCenter) * (1/renderScale)
-                            + volumeCenter[2];
-                    
-                for (double k = -depth; k <= depth; k += 5/renderScale) {
-                    pixelCoord[0] = originPlaneCoord[0] + viewVec[0] * k;
-                    pixelCoord[1] = originPlaneCoord[1] + viewVec[1] * k;
-                    pixelCoord[2] = originPlaneCoord[2] + viewVec[2] * k;
-                    
-                    val += getVoxel(pixelCoord);
-                }
-                
-                pixelValues[i * nativeImage.getHeight() + j] = val;
-            }
-        }
-        double max = 0; // Max is updated while calculating the pixel values
-        for (int i = 0; i < pixelValues.length; i++) {
-            max = Math.max(max, pixelValues[i]);
-        }
-        
-        for (int j = 0; j < nativeImage.getHeight(); j++) {
-            for (int i = 0; i < nativeImage.getWidth(); i++) {
-                double val = pixelValues[i * nativeImage.getHeight() + j];
-                max = Math.max(max, val);
-                
-                // Map the intensity to a grey value by linear scaling
-                voxelColor.r = val/max;
-                voxelColor.g = voxelColor.r;
-                voxelColor.b = voxelColor.r;
-                voxelColor.a = val > 0 ? 1.0 : 0.0;  // this makes intensity 0 completely transparent and the rest opaque
-                // Alternatively, apply the transfer function to obtain a color
-                voxelColor = tFunc.getColor((int) Math.floor(val / max));
-                
-                
-                // BufferedImage expects a pixel color packed as ARGB in an int
-                int c_alpha = voxelColor.a <= 1.0 ? (int) Math.floor(voxelColor.a * 255) : 255;
-                int c_red = voxelColor.r <= 1.0 ? (int) Math.floor(voxelColor.r * 255) : 255;
-                int c_green = voxelColor.g <= 1.0 ? (int) Math.floor(voxelColor.g * 255) : 255;
-                int c_blue = voxelColor.b <= 1.0 ? (int) Math.floor(voxelColor.b * 255) : 255;
-                int pixelColor = (c_alpha << 24) | (c_red << 16) | (c_green << 8) | c_blue;
-                nativeImage.setRGB(i, j, pixelColor);
-            }
-        }
-        
-        AffineTransform at = new AffineTransform();
-        at.scale(1/renderScale, 1/renderScale);
-        AffineTransformOp scaleOp = new AffineTransformOp(at, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-        
-        scaleOp.filter(nativeImage, image);
-    }
+//    void compositing(double[] viewMatrix){
+//        // clear image
+//        for (int j = 0; j < nativeImage.getHeight(); j++) {
+//            for (int i = 0; i < nativeImage.getWidth(); i++) {
+//                nativeImage.setRGB(i, j, 0);
+//            }
+//        }
+//
+//        // vector uVec and vVec define a plane through the origin, 
+//        // perpendicular to the view vector viewVec
+//        double[] viewVec = new double[3];
+//        double[] uVec = new double[3];
+//        double[] vVec = new double[3];
+//        VectorMath.setVector(viewVec, viewMatrix[2], viewMatrix[6], viewMatrix[10]);
+//        VectorMath.setVector(uVec, viewMatrix[0], viewMatrix[4], viewMatrix[8]);
+//        VectorMath.setVector(vVec, viewMatrix[1], viewMatrix[5], viewMatrix[9]);
+//        
+//        // image is square
+//        int imageCenter = nativeImage.getWidth() / 2;
+//
+//        double[] originPlaneCoord = new double[3];
+//        double[] pixelCoord = new double[3];
+//        double[] volumeCenter = new double[3];
+//        double[] pixelValues = new double[nativeImage.getHeight() * nativeImage.getWidth()];
+//        VectorMath.setVector(volumeCenter, volume.getDimX() / 2, volume.getDimY() / 2, volume.getDimZ() / 2);
+//        
+//        // sample on a plane through the origin of the volume data
+//        TFColor voxelColor = new TFColor();
+//
+//        
+//        for (int j = 0; j < nativeImage.getHeight(); j++) {
+//            for (int i = 0; i < nativeImage.getWidth(); i++) {
+//                double val = 0;
+//                
+//                int depth = Math.max(volume.getDimX(), Math.max(volume.getDimY(), volume.getDimZ()));
+//                originPlaneCoord[0] = uVec[0] * (i - imageCenter) * (1/renderScale) + vVec[0] * (j - imageCenter) * (1/renderScale)
+//                            + volumeCenter[0];
+//                originPlaneCoord[1] = uVec[1] * (i - imageCenter) * (1/renderScale) + vVec[1] * (j - imageCenter) * (1/renderScale)
+//                            + volumeCenter[1];
+//                originPlaneCoord[2] = uVec[2] * (i - imageCenter) * (1/renderScale) + vVec[2] * (j - imageCenter) * (1/renderScale)
+//                            + volumeCenter[2];
+//                    
+//                for (double k = -depth; k <= depth; k += 5/renderScale) {
+//                    pixelCoord[0] = originPlaneCoord[0] + viewVec[0] * k;
+//                    pixelCoord[1] = originPlaneCoord[1] + viewVec[1] * k;
+//                    pixelCoord[2] = originPlaneCoord[2] + viewVec[2] * k;
+//                    
+//                    val += getVoxel(pixelCoord);
+//                }
+//                
+//                pixelValues[i * nativeImage.getHeight() + j] = val;
+//            }
+//        }
+//        double max = 0; // Max is updated while calculating the pixel values
+//        for (int i = 0; i < pixelValues.length; i++) {
+//            max = Math.max(max, pixelValues[i]);
+//        }
+//        
+//        for (int j = 0; j < nativeImage.getHeight(); j++) {
+//            for (int i = 0; i < nativeImage.getWidth(); i++) {
+//                double val = pixelValues[i * nativeImage.getHeight() + j];
+//                max = Math.max(max, val);
+//                
+//                // Map the intensity to a grey value by linear scaling
+//                voxelColor.r = val/max;
+//                voxelColor.g = voxelColor.r;
+//                voxelColor.b = voxelColor.r;
+//                voxelColor.a = val > 0 ? 1.0 : 0.0;  // this makes intensity 0 completely transparent and the rest opaque
+//                // Alternatively, apply the transfer function to obtain a color
+//                voxelColor = tFunc.getColor((int) Math.floor(val / max));
+//                
+//                
+//                // BufferedImage expects a pixel color packed as ARGB in an int
+//                int c_alpha = voxelColor.a <= 1.0 ? (int) Math.floor(voxelColor.a * 255) : 255;
+//                int c_red = voxelColor.r <= 1.0 ? (int) Math.floor(voxelColor.r * 255) : 255;
+//                int c_green = voxelColor.g <= 1.0 ? (int) Math.floor(voxelColor.g * 255) : 255;
+//                int c_blue = voxelColor.b <= 1.0 ? (int) Math.floor(voxelColor.b * 255) : 255;
+//                int pixelColor = (c_alpha << 24) | (c_red << 16) | (c_green << 8) | c_blue;
+//                nativeImage.setRGB(i, j, pixelColor);
+//            }
+//        }
+//        
+//        AffineTransform at = new AffineTransform();
+//        at.scale(1/renderScale, 1/renderScale);
+//        AffineTransformOp scaleOp = new AffineTransformOp(at, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+//        
+//        scaleOp.filter(nativeImage, image);
+//    }
 
     public void setRayFunction(int functionName){
         this.rayFunction = functionName;
@@ -339,7 +373,7 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
                 mip(matrix);
                 break;
             case 2:
-                compositing(matrix);
+                //compositing(matrix);
                 break;
         }
     }
@@ -403,12 +437,12 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
 
     }
     
-    private void createNativeImage() {
-        nativeImage = new BufferedImage(
-            (int) Math.floor(image.getWidth() * renderScale),
-            (int) Math.floor(image.getHeight() * renderScale),
-            BufferedImage.TYPE_INT_ARGB);
-    }
+//    private void createNativeImage() {
+//        nativeImage = new BufferedImage(
+//            (int) Math.floor(image.getWidth() * renderScale),
+//            (int) Math.floor(image.getHeight() * renderScale),
+//            BufferedImage.TYPE_INT_ARGB);
+//    }
 
     @Override
     public void visualize(GL2 gl) {
@@ -466,12 +500,14 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
 
     }
     private BufferedImage image;
-    private BufferedImage nativeImage;
-    private double renderScale = 0.5;
+    // private BufferedImage nativeImage;
+    // private double renderScale = 0.5;
+    private int sampleNum = 80;
 
-    public void setRenderScale(double rs) {
-        renderScale = rs;
-        createNativeImage();
+    public void setSampleNum(int sn) {
+        sampleNum = sn;
+        callRayFunction(viewMatrix);
+        //createNativeImage();
     }
     
     private double[] viewMatrix = new double[4 * 4];
